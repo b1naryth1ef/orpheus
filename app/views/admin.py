9f1602f91756c465115fbbaa90023d716590f246
Andrei <b1naryth1ef@gmail.com>
2015-03-15 03:05:33 -0500

    Fix divide by zero error on admin index

--- app/views/admin.py
+++ app/views/admin.py
@@ -33,7 +33,8 @@ def admin_dashboard():
     rush_conn = redis.get("rush:open")
     bots_online = g.cursor.execute("SELECT count(*) as c FROM bots WHERE status='USED'").fetchone().c
     bot_used, bot_total = get_bot_space()
-    b_cap = 100 - (((float(bot_used or 0) / bot_total)) * 100)
+    if bot_total:
+        b_cap = 100 - (((float(bot_used or 0) / bot_total)) * 100)
     return render_template("admin/index.html",
         users_count=g.cursor.count("users"),
         games_count=g.cursor.count("games"),
