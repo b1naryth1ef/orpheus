2dfb4b7da87b1c68e7d74cffa221a3fe806a6198
max <vcemax@gmail.com>
2015-03-26 21:38:28 -0700

    Initial stats implementations
    Improved manage.py and generate.py facilities for match and bet
    generation to be a bit more realistic
    
    Added initial /stats page and api calls

---
+++ app/js/stats.js
@@ -0,0 +1,56 @@
+var stats = app.view("stats");
+
+stats.renderMatches = function(id) {
+    var renderMatchesFromData = (function (data) {
+       $(".events-container").empty();
+
+        _.each(data.matches, (function (item) {
+            $(".events-container").append(this.app.render("match_frontpage", {
+                match: item
+            }));
+        }).bind(this));
+    }).bind(this);
+
+    $.ajax("/api/events/" + id + "/list", {
+        success: renderMatchesFromData
+    })
+}
+
+stats.renderEvents = function () {
+    var renderEventsFromData = (function (data) {
+        $(".events-container").empty();
+
+        _.each(data.events, (function (item) {
+            $(".events-container").append(this.app.render("events_frontpage", {
+                event: item
+            }));
+        }).bind(this));
+    }).bind(this);
+
+    $.ajax("/api/events/list", {
+        type: 'POST',
+        data: {
+            active: true
+        },
+        success: renderEventsFromData
+    })
+}
+
+stats.route("/stats", function () {
+    this.renderEvents();
+	console.log("test");
+    $(".events-container").delegate(".event-row", "click", (function (ev) {
+        var eventID = $(ev.target).closest(".event-row").attr("data-id");
+        window.location = "/event/" + eventID;
+    }).bind(this));
+});
+
+stats.routeRegex(/^\/event\/(\d+)$/, function (route, id) {
+    this.renderMatches(id);
+
+    $(".events-container").delegate(".match-row", "click", (function (ev) {
+        var eventID = $(ev.target).closest(".match-row").attr("data-id");
+        window.location = "/match/" + eventID;
+    }).bind(this));
+});
+
