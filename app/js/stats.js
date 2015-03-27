a04f24cf749093d11c236c8cfbb0c8b60a3ad7ea
max <vcemax@gmail.com>
2015-03-26 23:47:15 -0700

    Added more analytics thinsg
    - Added temporary shortcut on the frontpage for stats
    - Added a basic line graph for bet history (cumulative amount won/lost
    over time); don't forget to regenerate the db to generate some bet
    history
    - Added a basic navbar

--- app/js/stats.js
+++ app/js/stats.js
@@ -3,17 +3,32 @@ var stats = app.view("stats");
 stats.renderStats = function(){
 	var renderStatsFromData = (function(data){
 		$(".stats-container").html(this.app.render("stats_page", {}));
-		$(function(){
-			var history_data = [];
-			var cumulative = 0;
-			for(var i = 0; i < data.history.length; i++){
-				history_data.push([i, cumulative]);
-				cumulative += data.history[i].value * (data.history[i].won? 1 : -1);
-				console.log(data.history[i].match_date);
-			}
+		var history_data = [];
+		var cumulative = 0;
+		for(var i = 0; i < data.history.length; i++){
+			history_data.push([i, cumulative]);
+			cumulative += data.history[i].value * (data.history[i].won? 1 : -1);
+		}
+		//$("#stats-history-display").width(1000);
 
-			$.plot("#stats-history-display", [history_data]);
-		});     
+		$("#stats-history-display").height(750);
+		$(".stats-container").resizable({
+			maxWidth:1000,
+			maxHeight:750,
+			minWidth:450,
+			minHeight:100
+		});
+		$.plot("#stats-history-display", [history_data]);
+
+		$("#value-history").click((function(){
+			console.log("value-history");
+		}).bind(this));
+		$("#bet-win-loss").click((function(){
+			console.log("bet-win-loss");
+		}).bind(this));
+		$("#help").click((function(){
+			console.log("help");
+		}).bind(this));
 
 	}).bind(this);
 
